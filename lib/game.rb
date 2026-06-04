835a7de5dac93a11c8e8d8bb84f62a2549398a44
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-06-04 15:33:26 -0400

    Implement initialize in game

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,10 @@
+require_relative 'deck'
+
 class Game
+  attr_reader :players, :deck
+
+  def initialize(player_objs)
+    @players = player_objs
+    @deck = Deck.new
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,24 @@
 require_relative '../lib/game'
+require_relative '../lib/player'
+require_relative '../lib/deck'
 
 describe Game do
+  # creates a deck
+  # deals the deck between the two players
+  # the deck should be shuffled
+  describe '#initialize' do
+    let(:player1) { Player.new('Jeff') }
+    let(:player2) { Player.new('Bob') }
+    let(:players) { [player1, player2] }
+
+    let(:game) { described_class.new(players) }
+
+    it 'contains an array of players' do
+      expect(game.players).to eq players
+    end
+
+    it 'creates a deck of cards' do
+      expect(game.deck).to be_a(Deck)
+    end
+  end
 end
