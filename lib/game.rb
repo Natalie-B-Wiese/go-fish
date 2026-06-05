53002c691cd0755ce71620dab0cc435a7918d970
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-06-05 08:24:20 -0400

    Begin writing request deck card scenarios

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,12 @@ require_relative 'deck'
 
 class Game
   attr_reader :players, :deck
+  attr_accessor :current_player_index
 
   def initialize(player_objs)
     @players = player_objs
     @deck = Deck.new
+    @current_player_index = 0
   end
 
   def start
@@ -13,7 +15,26 @@ class Game
     deal
   end
 
+  def request_deck_card(rank)
+    if deck.empty?
+      switch_turn
+    else
+      card_taken = deck.take_top_card
+      if card_taken.rank == rank
+        puts 'TODO: player received card they wanted'
+      else
+        puts 'TODO: give player their card'
+        switch_turn
+      end
+    end
+  end
+
   private
+
+  def switch_turn
+    self.current_player_index += 1
+    self.current_player_index = 0 if current_player_index >= players.length
+  end
 
   def deal
     cards_per_player = players.length <= 3 ? 7 : 5
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/game'
 require_relative '../lib/player'
 require_relative '../lib/deck'
+require_relative '../lib/card'
 
 describe Game do
   # creates a deck
@@ -19,6 +20,10 @@ describe Game do
 
     it 'creates a deck of cards' do
       expect(game.deck).to be_a(Deck)
+    end
+
+    it 'sets current_player_index to 0' do
+      expect(game.current_player_index).to eq 0
     end
   end
 
@@ -105,6 +110,144 @@ describe Game do
         expect(player2.cards).to_not eq p2_unshuffled
         expect(player3.cards).to_not eq p3_unshuffled
         expect(player4.cards).to_not eq p4_unshuffled
+      end
+    end
+  end
+
+  describe '#request_deck_card' do
+    let(:player1) { Player.new('Jeff') }
+    let(:player2) { Player.new('Bob') }
+    let(:players) { [player1, player2] }
+
+    let(:game) { described_class.new(players) }
+
+    let(:ace_spades) { Card.new('A', 'Spades') }
+    let(:ace_clubs)  { Card.new('A', 'Clubs') }
+    let(:ace_hearts) { Card.new('A', 'Hearts') }
+
+    let(:ace_diamonds) { Card.new('A', 'Diamonds') }
+    let(:other_card) { Card.new('5', 'Spades') }
+
+    let(:player1_index) { 0 }
+    let(:player2_index) { 1 }
+
+    context 'when deck is empty' do
+      before do
+        game.deck.cards = []
+      end
+
+      it 'switches turn' do
+        game.request_deck_card('A')
+        expect(game.current_player_index).to eq player2_index
+      end
+    end
+
+    context 'when player does not get correct card from deck' do
+      before do
+        player1.add_cards([ace_spades, ace_clubs, ace_hearts])
+        game.deck.cards = [other_card, ace_diamonds]
+      end
+
+      it 'switches turn' do
+        game.request_deck_card('A')
+        expect(game.current_player_index).to eq player2_index
+      end
+    end
+
+    xcontext 'when player has cards and deck has cards' do
+      before do
+        player1.add_cards([ace_spades, ace_clubs, ace_hearts])
+        game.deck.cards = [ace_diamonds, other_card]
+      end
+
+      it 'removes the card from the deck' do
+        card_count_before = game.deck.cards.length
+        game.request_deck_card('A')
+        expect(game.deck.cards.length).to eq(card_count_before - 1)
+      end
+
+      it 'adds the card to the player' do
+        card_count_before = player1.cards.length
+        game.request_deck_card('A')
+        expect(player1.cards.length).to eq(card_count_before + 1)
+      end
+
+      context 'when player gets correct card from deck' do
+        it 'makes player have another turn' do
+          player1_index = 0
+
+          game.request_deck_card('A')
+          expect(game.current_player_index).to eq player1_index
+        end
+      end
+    end
+
+    xcontext 'when player does not have cards' do
+      context 'when there are cards in the deck' do
+      end
+
+      context 'when the deck is empty' do
+        # player is out of the game
+      end
+    end
+  end
+
+  xdescribe '#play_round' do
+    let(:player1) { Player.new('Jeff') }
+    let(:player2) { Player.new('Bob') }
+    let(:players) { [player1, player2] }
+
+    let(:game) { described_class.new(players) }
+
+    before do
+      # forcefully set the hands
+      ace_spades = Card.new('A', 'Spades')
+      ace_clubs = Card.new('A', 'Clubs')
+      ace_hearts = Card.new('A', 'Hearts')
+      ace_diamonds = Card.new('A', 'Diamonds')
+
+      jack_spades = Card.new('Jack', 'Spades')
+      jack_clubs = Card.new('Jack', 'Clubs')
+      jack_hearts = Card.new('Jack', 'Hearts')
+
+      jack_diamonds = Card.new('Jack', 'Diamonds')
+
+      other_card = Card.new('5', 'Spades')
+
+      player1.add_cards([ace_spades, ace_clubs, ace_hearts, jack_spades])
+      player2.add_cards([jack_clubs, jack_hearts, ace_diamonds])
+
+      game.deck.cards = [other_card, jack_diamonds]
+    end
+
+    context 'when player has cards' do
+      context 'when player gets correct card from opponent' do
+        it 'gives player the card from opponent' do
+          game.play_round
+
+          # how to make request of player 1 for
+        end
+
+        it 'allows player to go again' do
+        end
+      end
+
+      context 'when player goes fish and gets correct card from deck' do
+      end
+
+      context 'when player goes fish and deck is empty' do
+      end
+
+      context 'when player gets correct card and can make a book' do
+      end
+    end
+
+    context 'when player does not have cards' do
+      context 'when there are cards in the deck' do
+      end
+
+      context 'when the deck is empty' do
+        # player is out of the game
       end
     end
   end
